e08c10414e2b7270321458a2af3acfbaed9a05a8
eboboeing-747 <silinskiymihail@gmail.com>
2025-11-26 20:50:00 +0300

    make estate_object_to_image a required_one_to_many
    make EstateObjectImageEntity.Name optional

--- backend/DmsDb/Entity/EstateObjectImageEntity.cs
+++ backend/DmsDb/Entity/EstateObjectImageEntity.cs
@@ -3,9 +3,9 @@ namespace DmsDb.Entity;
 public class EstateObjectImageEntity
 {
     public Guid Id { get; set; }
-    public Guid? EstateObjectId { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public Guid EstateObjectId { get; set; }
+    public string? Name { get; set; } = string.Empty;
     public string Path { get; set; } = string.Empty;
 
-    public EstateObjectEntity? EstateObject { get; } = null;
+    public EstateObjectEntity EstateObject { get; } = null!;
 }
